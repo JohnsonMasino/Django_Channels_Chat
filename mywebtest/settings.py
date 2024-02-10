2efd0e3dd2c6737a3f3fb38947e9eaa59f02a279
Johnson Masino <johnsonmasino@gmail.com>
2024-02-10 21:41:17 +0100

    A simple modification on settings

--- mywebtest/settings.py
+++ mywebtest/settings.py
@@ -31,12 +31,16 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'chat.apps.ChatConfig',
+
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'channels',
 ]
 
 MIDDLEWARE = [
