3a554444fc244041e62e3789df997e61ff943813
Johnson Masino <johnsonmasino@gmail.com>
2024-02-12 06:28:07 +0100

    Updating The Notification System

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # This is a chat system with django channels and websockets
+# Notification System
--- mywebtest/settings.py
+++ mywebtest/settings.py
@@ -30,6 +30,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'chat.apps.ChatConfig',
+    'notifications.apps.NotificationsConfig',
 ]
 
 ASGI_APPLICATION = 'mywebtest.asgi.application'
