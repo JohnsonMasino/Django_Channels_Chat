7489ab102481e8bdbeaa652de497999b6afe973c
Johnson Masino <johnsonmasino@gmail.com>
2024-02-18 11:51:08 +0100

    Added some comments

--- chat/views.py
+++ chat/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
 
+# the view function for the chat page
 def chat_page(request):
     return render(request, 'chat/chat_page.html')
--- mywebtest/urls.py
+++ mywebtest/urls.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('chat.urls')), # the chat url
-    path('notifications/', include('notifications.urls')),
+    path('admin/', admin.site.urls), # the url for the admin site
+    path('', include('chat.urls')), # the url for the chat app
+    path('notifications/', include('notifications.urls')), # the url for the notifications app
 ]
